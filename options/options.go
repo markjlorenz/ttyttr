46100a4a4db5be831b22820dd2c2f474cfdc0e17
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2014-02-19 22:56:59 -0500

    move the goodies to options

--- options/options.go
+++ options/options.go
@@ -1,30 +1,36 @@
-// package opts
-//
-// import (
-//   "flag"
-//   "strings"
-//   "fmt"
-//   "os"
-// )
-//
-// type Options struct {
-//   Blueprint []string
-// }
-//
-// func (o *Options) Parse() {
-//   flag.Usage = usage
-//   default_blueprint := "PRP VBD NN IN NNS DT NN VBG PRP$ NN"
-//   var blueprint string
-//
-//   flag.StringVar(&blueprint, "blueprint", default_blueprint, "the sentence structure to be output")
-//   flag.Parse()
-//   o.Blueprint = strings.Fields(blueprint)
-// }
-//
-// func usage() {
-//   fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-//   fmt.Fprintln(os.Stderr, "Feed the corpus into stdin.")
-//   flag.PrintDefaults()
-//   fmt.Fprintln(os.Stderr, "Example: ")
-//   fmt.Fprintln(os.Stderr, "  talky < corpus.txt")
-// }
+package opts
+
+import (
+  "flag"
+  "fmt"
+  "os"
+)
+
+type Options struct {
+  Endpoint string
+  Bearer   string
+  Tweet    string
+}
+
+func (o *Options) Parse() {
+  flag.Usage = usage
+  endpoint  := "https://api.twitter.com/1.1/statuses/show/"
+  bearer    := "AAAAAAAAAAAAAAAAAAAAAGrhWAAAAAAAZzMgE%2BKmTuprORfq%2BDkzDe7d4Hc%3Dc0AYgrQHlsoY2Ag22r4c1RtFfCwSS0d9C0WL063T5QSPZGaS1T"
+
+  flag.StringVar(&endpoint, "endpoint", endpoint, "twitter api endpoint")
+  flag.StringVar(&bearer, "bearer", bearer, "twitter bearer token")
+  flag.Parse()
+
+  o.Endpoint = endpoint
+  o.Bearer   = bearer
+  o.Tweet    = flag.Arg(0)
+}
+
+func usage() {
+  fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+  fmt.Fprintln(os.Stderr, "Takes one position argument, a tweet id")
+  flag.PrintDefaults()
+  fmt.Fprintln(os.Stderr, "Example: ")
+  fmt.Fprintln(os.Stderr, "  ttyttr 434135040256008192")
+  fmt.Fprintln(os.Stderr, "  requests a tweet from: https://twitter.com/someuser/status/434135040256008192")
+}
--- ttyttr.go
+++ ttyttr.go
@@ -2,19 +2,18 @@ package main
 
 import (
   "dapplebeforedawn/ttyttr/api"
+  "dapplebeforedawn/ttyttr/options"
   "fmt"
-  // "dapplebeforedawn/ttyttr/options"
 )
 
 func main() {
-  // opts := opts.Options{}
-  // opts.Parse()
-  // id       := "436310137683197953"
-  id       := "434135040256008192"
-  endpoint := "https://api.twitter.com/1.1/statuses/show/"
-  bearer   := "AAAAAAAAAAAAAAAAAAAAAGrhWAAAAAAAZzMgE%2BKmTuprORfq%2BDkzDe7d4Hc%3Dc0AYgrQHlsoY2Ag22r4c1RtFfCwSS0d9C0WL063T5QSPZGaS1T"
+  opts := opts.Options{}
+  opts.Parse()
 
-  tweet := api.NewTweet(id, endpoint, bearer)
-  data := tweet.Get()
+  // id       := "436310137683197953"
+  // id       := "434135040256008192"
+
+  tweet := api.NewTweet(opts.Tweet, opts.Endpoint, opts.Bearer)
+  data  := tweet.Get()
   fmt.Println(data)
 }
